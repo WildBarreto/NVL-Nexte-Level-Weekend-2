ef5a14ee3822f32ce4fe7e493ed7a1b1e76cc4b6
wild santos barreto <wildsbsrreto@gmail.com>
2020-08-09 13:51:47 -0300

    Add exisiting file

---
+++ server/src/server.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(routes);
+
+
+app.listen(3333);
+
+
+
+// GET: buscar ou listar uma infromação
+// Post: Criar alguma nova informação
+// PUT: Atualizar uma informação existente
+// DELETE: Deletar uma informação existente 
+
+// Corpo (Request Body): Dados para criação ou atualização de um registro
+// Route Params: identificar qual recurso eu quero atualizar ou deletar
+// Query Params:
